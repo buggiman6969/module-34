c6c15bfb21627cc3d506cb6f0c83bf77e5658dc6
buggiman6969 <abdullaapon12@gmail.com>
2023-03-03 16:01:10 +0600

    first commit

--- js/country.js
+++ js/country.js
@@ -13,11 +13,28 @@ const displayCountries = countries =>{
         countryDiv.innerHTML = `
         <h3>Name: ${country.name.common}</h3>
         <p><i> Capital: ${ country.capital}</i></p>
-        <button class="btn">Details</button>
+        <button class="btn" onclick="loadCountryDetails('${country.cca2}')">Details</button>
         ` 
         countryContainer.appendChild(countryDiv)
         
     });
 }
 
+const loadCountryDetails =code =>{
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showCountryDetails(data[0]))
+
+}
+
+const showCountryDetails = country =>{
+    const showCountryDetails = document.getElementById('country-details')
+    showCountryDetails.innerHTML = `
+    <h3>name: ${country.name.common}</h3>
+    <img src="${country.flags.png}"> <br>
+    <a href="https://restcountries.com/v3.1/alpha/${country.cca2}" target="_blank">country details</a>
+    `
+}
 loaCountries();
